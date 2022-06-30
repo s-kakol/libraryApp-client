58d13b3136bfe2143ad7e6488dde2b99b3212c7a
s-kakol <kakolsylwester@gmail.com>
2022-06-30 11:07:22 +0200

    Refactor: Improve book service usability

--- src/utilities/services/books.ts
+++ src/utilities/services/books.ts
@@ -1,17 +1,17 @@
 import axios from 'axios';
 import Book from '../types/book.type';
-const baseUrl = 'http://localhost:3001';
+const baseUrl = 'http://localhost:3001/books';
 
-export const getAll = async (): Promise<Book[]> => {
-  const response = await axios.get(`${baseUrl}/books`);
+const getAll = async (): Promise<Book[]> => {
+  const response = await axios.get(`${baseUrl}`);
   return response.data;
 };
 
-export const getMostPopular = async (length?: number): Promise<Book[]> => {
+const getMostPopular = async (length?: number): Promise<Book[]> => {
   if (!length) {
     length = 10;
   }
-  const response = await axios.get(`${baseUrl}/books`);
+  const response = await axios.get(`${baseUrl}`);
   const result = response.data
     .sort((previous: Book, next: Book) => {
       return next.rating - previous.rating;
