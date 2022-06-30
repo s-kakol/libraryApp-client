b0bbd4328fa2b52e4e35342b3205f3b2f36c8faa
s-kakol <kakolsylwester@gmail.com>
2022-06-30 15:34:32 +0200

    Fix: Correct error handling in Book Details

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -11,14 +11,14 @@ import Image from 'react-bootstrap/Image';
 const BookDetails = (): JSX.Element => {
   const id = useParams().id || '';
   const [book, setBook] = useState<Book>();
-  const [error, setError] = useState<string>();
+  const [error, setError] = useState<boolean>(false);
 
   const initBookDetails = async (id: string) => {
-    const result = await bookService.getOneById(id);
-    if (result instanceof Error) {
-      setError(result.message);
-    } else {
+    try {
+      const result = await bookService.getOneById(id);
       setBook(result);
+    } catch {
+      setError(true);
     }
   };
 
@@ -37,7 +37,6 @@ const BookDetails = (): JSX.Element => {
       alignSelf: 'center',
     };
 
-    console.log(book);
     return (
       <Container>
         <Row style={{ margin: '3rem auto' }}>
