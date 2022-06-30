12e9457f8449c10d9565cde2316fa54c5e8d030b
s-kakol <kakolsylwester@gmail.com>
2022-06-30 08:59:59 +0200

    Feat: Add types to book service functionality

--- src/utilities/services/books.ts
+++ src/utilities/services/books.ts
@@ -2,18 +2,18 @@ import axios from 'axios';
 import Book from '../types/book.type';
 const baseUrl = 'http://localhost:3001';
 
-export const getAll = async () => {
+export const getAll = async (): Promise<Book[]> => {
   const response = await axios.get(`${baseUrl}/books`);
   return response.data;
 };
 
-export const getMostPopular = async (length?: number) => {
+export const getMostPopular = async (length?: number): Promise<Book[]> => {
   if (!length) {
     length = 10;
   }
   const response = await axios.get(`${baseUrl}/books`);
   const result = response.data
-    .sort((previous, next) => {
+    .sort((previous: Book, next: Book) => {
       return next.rating - previous.rating;
     })
     .slice(0, length);
