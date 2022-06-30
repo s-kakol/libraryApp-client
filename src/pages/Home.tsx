19617a7e39b137b50222c5e28708bdb4304b685f
s-kakol <kakolsylwester@gmail.com>
2022-06-30 10:52:17 +0200

    Feat: Finish Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,40 @@
+import { useEffect, useState } from 'react';
+import Carousel from '../components/Carousel';
+import NewsList from '../components/NewsList';
+import bookService from '../utilities/services/books';
+import newsService from '../utilities/services/news';
+import Book from '../utilities/types/book.type';
+import NewsItem from '../utilities/types/news.type';
+
 const Home = (): JSX.Element => {
-  return <h1>Home</h1>;
+  const [popularBooks, setPopularBooks] = useState<Book[]>([]);
+  const [newsList, setNewsList] = useState<NewsItem[]>([]);
+
+  const initPopularBooks = async () => {
+    const howMany = 9;
+    const result = await bookService.getMostPopular(howMany);
+    setPopularBooks(result);
+  };
+
+  const initNews = async () => {
+    const result = await newsService.getAll();
+    setNewsList(result);
+  };
+
+  useEffect(() => {
+    initPopularBooks();
+    initNews();
+  }, []);
+
+  return (
+    <>
+      <div style={{ margin: '2rem' }}>
+        <h3 className="text-center">Recommended by our readers</h3>
+        <Carousel books={popularBooks} />
+      </div>
+      <NewsList news={newsList} />
+    </>
+  );
 };
 
 export default Home;
