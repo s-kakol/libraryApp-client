01e6ba9e41925036a1f3483a7e42bdcff2091342
s-kakol <kakolsylwester@gmail.com>
2022-06-30 22:33:42 +0200

    Feat: Improve books list rendering in Book page

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { CardGroup, Col, Container, Row } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import BookCard from '../components/book/BookCard';
 import bookService from '../utilities/services/books';
@@ -7,6 +8,7 @@ import Book from '../utilities/types/book.type';
 const Books = (): JSX.Element => {
   const genre = useParams().genre || null;
   const [books, setBooks] = useState<Book[]>([]);
+  const subArraySize = 5;
 
   const initBooksList = async () => {
     let booksList;
@@ -22,12 +24,27 @@ const Books = (): JSX.Element => {
     initBooksList();
   }, [genre]);
 
+  const subArray = (arr: Book[], size: number): Book[][] =>
+    [...Array(Math.ceil(arr.length / size))].map((_, i) =>
+      arr.slice(size * i, size + size * i)
+    );
+
+  const bookGroups = subArray(books, subArraySize);
+
   return (
-    <>
-      {books.map(book => (
-        <BookCard key={book.id} book={book} />
+    <Container>
+      {bookGroups.map((booksSubGroup, i) => (
+        <Row key={i}>
+          <Col>
+            <CardGroup>
+              {booksSubGroup.map(book => (
+                <BookCard key={book.id} book={book} />
+              ))}
+            </CardGroup>
+          </Col>
+        </Row>
       ))}
-    </>
+    </Container>
   );
 };
 
