c7482f1414d0f2f9fb2c74813b8f3b2e60161de3
s-kakol <kakolsylwester@gmail.com>
2022-07-01 12:42:34 +0200

    Feat: Add form validation for Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -7,23 +7,70 @@ import Overlay from 'react-bootstrap/Overlay';
 import Tooltip from 'react-bootstrap/Tooltip';
 import './Login.scss';
 import { useRef, useState } from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 const Login = (): JSX.Element => {
   const [show, setShow] = useState<boolean>(false);
   const target = useRef(null);
 
+  const errorStyle = {
+    color: '#ce2649',
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      loginEmail: '',
+      loginPassword: '',
+    },
+    onSubmit: values => {
+      console.log(values);
+    },
+    validationSchema: Yup.object({
+      loginEmail: Yup.string()
+        .email('Invalid email address')
+        .max(320, 'Email is too long')
+        .required('Please enter email'),
+      loginPassword: Yup.string()
+        .max(128, 'Password is too long')
+        .required('Please enter password'),
+    }),
+  });
+
   return (
     <Container style={{ maxWidth: '50vh', padding: '3rem' }}>
       <Row>
-        <Form>
+        <Form noValidate onSubmit={formik.handleSubmit}>
           <Form.Group className="mb-3" controlId="loginEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Label>
+              <strong>Email address</strong>
+            </Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.loginEmail}
+            />
+            {formik.touched.loginEmail && formik.errors.loginEmail ? (
+              <p style={errorStyle}>{formik.errors.loginEmail}</p>
+            ) : null}
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="loginPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Label>
+              <strong>Password</strong>
+            </Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
+              value={formik.values.loginPassword}
+            />
+            {formik.touched.loginPassword && formik.errors.loginPassword ? (
+              <p style={errorStyle}>{formik.errors.loginPassword}</p>
+            ) : null}
           </Form.Group>
           <Row>
             <Col>
